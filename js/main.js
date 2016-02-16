3689f40b54b0d4f2e84238e20ff249966a847b96
Jim <jimcen33@gmail.com>
2016-02-16 16:26:14 +0800

    First commit for phone gap tutorial.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var app = {
+
+  findByName: function() {
+    var self = this;
+    this.store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(self.employeeLiTpl(employees));
+      });
+    },
+
+    initialize: function() {
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {
+        self.renderHomeView();
+        });
+    },
+
+    showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+     } else {
+        alert(title ? (title + ": " + message) : message);
+     }
+    },
+    renderHomeView: function() {
+      $('body').html(this.homeTpl());
+      $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+};
+
+app.initialize();
